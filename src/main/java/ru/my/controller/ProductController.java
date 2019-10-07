ba98b94438fda11d1948300ebb7ff03d8595003b
anderson178 <denismironenko@yandex.ru>
2019-10-07 18:11:03 +0300

    change modul Product

--- src/main/java/ru/my/controller/ProductController.java
+++ src/main/java/ru/my/controller/ProductController.java
@@ -10,8 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class ProductController {
-
-
     private ProductDAO productDAO = new ProductDAO();
 
     @GetMapping(value = "/getAll")
--- src/main/java/ru/my/model/Product.java
+++ src/main/java/ru/my/model/Product.java
@@ -1,22 +1,12 @@
 package ru.my.model;
 
 import lombok.*;
-import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
-import java.util.Comparator;
-
-@NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Product implements Serializable, Comparable<Product> {
+public class Product implements Comparable<Product> {
     private Integer position;
     private String name;
-
-//    @Override
-//    public int compare(Product o1, Product o2) {
-//        return o1.getPosition() - o2.getPosition();
-//    }
 
     @Override
     public int compareTo(Product o) {
